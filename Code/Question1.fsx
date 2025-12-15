83461c05df3e5ef08f71a34c190769467cca89d6
Sebastian-Francis-Taylor <me@sebastian-taylor.com>
2025-12-15 10:12:22 +0100

    slight changes

--- Code/Question1.fsx
+++ Code/Question1.fsx
@@ -4,8 +4,12 @@ let foo = printfn "bar"
 // 1.2
 let myFunction = printfn "Hello Exam time"
 
-
 let test x =
     match x with
     | 1 -> printfn "foo"
     | _ -> printfn "bar"
+
+let rec foldBack op list e =
+    match list with
+    | [] -> e
+    | (x :: xs) -> op x (foldBack op xs e)
